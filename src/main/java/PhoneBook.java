662226513185181c2bc38e3c3300ad10e4dfb9b9
Jonathan Leczner <jonathanleczner@Jonathan-ZCW-16.local>
2016-09-30 11:41:24 -0400

    updated UML, added stubs

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,6 +1,42 @@
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * Created by jonathanleczner on 9/30/16.
  */
 public class PhoneBook {
-    
+    private Map<String, List<Integer>> entries;
+
+    public PhoneBook() {
+        entries = new TreeMap<>();
+    }
+
+    public List<Integer> lookup(String name) {
+        return null;
+    }
+
+    public String reverseLookup(int phoneNumber) {
+        return null;
+    }
+
+    public void addEntry(String name, int phoneNumber) {
+        return;
+    }
+
+    public void removeEntry(String name, int phoneNumber) {
+        return;
+    }
+
+    public void removeRecord(String name) {
+        return;
+    }
+
+    public void listAllNames() {
+        return;
+    }
+
+    public void listAllEntries() {
+        return;
+    }
 }
