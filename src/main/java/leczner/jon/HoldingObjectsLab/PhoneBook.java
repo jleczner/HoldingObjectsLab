bda7a1437f49245f90f132d30cae7b75cc19b59c
Jonathan Leczner <jonathanleczner@Jonathan-ZCW-16.local>
2016-09-30 12:02:09 -0400

    added package, wrote implementation for addEntry and lookup

--- src/main/java/leczner/jon/HoldingObjectsLab/PhoneBook.java
+++ src/main/java/leczner/jon/HoldingObjectsLab/PhoneBook.java
@@ -1,3 +1,6 @@
+package leczner.jon.HoldingObjectsLab;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -13,7 +16,12 @@ public class PhoneBook {
     }
 
     public List<Integer> lookup(String name) {
-        return null;
+        if (entries.containsKey(name)) {
+            return entries.get(name);
+        } else {
+            System.out.println("Entry " + name + " not found");
+            return null;
+        }
     }
 
     public String reverseLookup(int phoneNumber) {
@@ -21,7 +29,12 @@ public class PhoneBook {
     }
 
     public void addEntry(String name, int phoneNumber) {
-        return;
+        List<Integer> numbers = entries.get(name);
+        if (numbers == null) {
+            numbers = new ArrayList<>();
+        }
+        numbers.add(phoneNumber);
+        entries.put(name, numbers);
     }
 
     public void removeEntry(String name, int phoneNumber) {
---
+++ src/test/java/leczner/jon/HoldingObjectsLab/PhoneBookTest.java
@@ -0,0 +1,35 @@
+package leczner.jon.HoldingObjectsLab;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+/**
+ * Created by jonathanleczner on 9/30/16.
+ */
+public class PhoneBookTest {
+    PhoneBook pb = new PhoneBook();
+
+    @Test
+    public void addEntryTest() {
+        pb.addEntry("Bob Jones", 1234567);
+        assertNotNull(pb.lookup("Bob Jones"));
+    }
+
+    @Test
+    public void lookupFailTest() {
+        assertNull(pb.lookup("Bob Jones"));
+    }
+
+    @Test
+    public void lookupTest() {
+        pb.addEntry("Bob Jones", 1234567);
+        pb.addEntry("Bob Jones", 3216547);
+        List<Integer> actual = pb.lookup("Bob Jones");
+        assertEquals(1234567, actual.get(0).intValue());
+    }
+}
